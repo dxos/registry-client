41f823bc0d465bdd84320aa2b1b5b4956156399c
Egor Gripasov <gripasovegor@gmail.com>
2019-03-16 14:13:37 -0400

    add more utils.

--- src/util.js
+++ src/util.js
@@ -2,6 +2,18 @@
 // Copyright 2019 Wireline, Inc.
 //
 
+if (typeof btoa === 'undefined') {
+  global.btoa = function (str) {
+    return new Buffer(str, 'binary').toString('base64');
+  };
+}
+
+if (typeof atob === 'undefined') {
+  global.atob = function (b64Encoded) {
+    return new Buffer(b64Encoded, 'base64').toString('binary');
+  };
+}
+
 /**
  * Utils
  */
