feb546f4789631444821b5ecac2edbbcbea122ee
Maximiliano Fierro <elmasse@gmail.com>
2019-03-20 16:22:40 -0300

    Fixes for building dist for node.

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,12 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: { chrome: "60" } }]
+            ]
+          }
         }
       },
 
