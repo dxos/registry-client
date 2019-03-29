831415021479f6d4c09006892516e41d33daac14
Egor Gripasov <gripasovegor@gmail.com>
2019-03-29 10:07:35 -0400

    warning fix

--- src/util.js
+++ src/util.js
@@ -4,13 +4,13 @@
 
 if (typeof btoa === 'undefined') {
   global.btoa = function(str) {
-    return new Buffer(str, 'binary').toString('base64');
+    return Buffer.from(str, 'binary').toString('base64');
   };
 }
 
 if (typeof atob === 'undefined') {
   global.atob = function(b64Encoded) {
-    return new Buffer(b64Encoded, 'base64').toString('binary');
+    return Buffer.from(b64Encoded, 'base64').toString('binary');
   };
 }
 
