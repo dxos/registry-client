e21ac311a79da577c3e11e60b95e00c718be2f22
Ashwin Phatak <ashwinpphatak@gmail.com>
2019-11-21 17:23:29 +0530

    Send coins API.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { RegistryClient } from './registry_client';
 import { Account } from './account';
 import { Util } from './util';
 import { TxBuilder } from './txbuilder';
-import { Msg, Record } from './types';
+import { Msg, Record, MsgSend } from './types';
 
 const CHAIN = 'wireline';
 const GQL_PATH = '/graphql';
@@ -88,7 +88,7 @@ export class Registry {
       if (process.env.MOCK_SERVER) {
         result = await this._client.insertRecord(record);
       } else {
-        result = await this._submit(privateKey, record, 'nameservice/SetRecord', transactionPrivateKey);
+        result = await this._submitRecordTx(privateKey, record, 'nameservice/SetRecord', transactionPrivateKey);
       }
     } catch (err) {
       const error = err[0] || err;
@@ -104,17 +104,29 @@ export class Registry {
    * @param {string} transactionPrivateKey - private key in HEX to sign transaction.
    */
   async deleteRecord(privateKey, record, transactionPrivateKey) {
-    return this._submit(privateKey, record, 'nameservice/DeleteRecord', transactionPrivateKey);
+    return this._submitRecordTx(privateKey, record, 'nameservice/DeleteRecord', transactionPrivateKey);
   }
 
   /**
-   * Mutate registry.
+   * Send coins.
+   * @param {object[]} amount
+   * @param {string} toAddress
+   * @param {string} privateKey
+   */
+  async sendCoins(amount, toAddress, privateKey) {
+    const account = new Account(Buffer.from(privateKey, 'hex'));
+    const fromAddress = account.formattedCosmosAddress;
+    return this._submitTx(new MsgSend(fromAddress, toAddress, amount), privateKey);
+  }
+
+  /**
+   * Submit record transaction.
    * @param {string} privateKey - private key in HEX to sign message.
    * @param {object} record
    * @param {string} operation
    * @param {string} transactionPrivateKey - private key in HEX to sign transaction.
    */
-  async _submit(privateKey, record, operation, transactionPrivateKey) {
+  async _submitRecordTx(privateKey, record, operation, transactionPrivateKey) {
     if (!privateKey.match(/^[0-9a-fA-F]{64}$/)) {
       throw new Error('Registry privateKey should be a hex string.');
     }
@@ -147,6 +159,33 @@ export class Registry {
     const tx = btoa(JSON.stringify(transaction, null, 2));
 
     // 4. Send transaction.
+    return this._client.submit(tx);
+  }
+
+  /**
+   * Submit a generic Tx to the chain.
+   * @param {object} message
+   * @param {string} privateKey - private key in HEX to sign transaction.
+   */
+  async _submitTx(message, privateKey) {
+    // Check private key.
+    if (!privateKey.match(/^[0-9a-fA-F]{64}$/)) {
+      throw new Error('Registry privateKey should be a hex string.');
+    }
+
+    // Check that the account exists on-chain.
+    const account = new Account(Buffer.from(privateKey, 'hex'));
+    const accountDetails = await this.getAccounts([account.formattedCosmosAddress]);
+    if (!accountDetails.length) {
+      throw new Error('Can not sign the transaction - account does not exist.');
+    }
+
+    // Generate signed Tx.
+    const { number, sequence } = accountDetails[0];
+    const transaction = TxBuilder.createTransaction(message, account, number.toString(), sequence.toString(), CHAIN);
+    const tx = btoa(JSON.stringify(transaction, null, 2));
+
+    // Submit Tx to chain.
     return this._client.submit(tx);
   }
 }
