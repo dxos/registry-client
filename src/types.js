25abe70f2246897fa0b99a4176e065790fc9126e
Ashwin Phatak <ashwinpphatak@gmail.com>
2019-11-21 17:23:29 +0530

    Send coins API.

--- src/types.js
+++ src/types.js
@@ -122,6 +122,42 @@ export class Msg {
 }
 
 /**
+ * Send-coins message from cosmos-sdk bank module.
+ */
+export class MsgSend {
+
+  /**
+   * @constructor
+   * @param {string} fromAddress
+   * @param {string} toAddress
+   * @param {object[]} amount
+   */
+  constructor(fromAddress, toAddress, amount) {
+    console.assert(fromAddress);
+    console.assert(toAddress);
+    console.assert(amount && amount.length);
+
+    this._fromAddress = fromAddress;
+    this._toAddress = toAddress;
+    this._amount = amount;
+  }
+
+  /**
+   * Serialize Message.
+   */
+  serialize() {
+    return Util.sortJSON({
+      'type': 'cosmos-sdk/MsgSend',
+      'value': {
+        'from_address': this._fromAddress,
+        'to_address': this._toAddress,
+        'amount': this._amount
+      }
+    });
+  }
+}
+
+/**
  * Transaction.
  */
 export class Transaction {
