b8608a2d160b950ffe2797497870283bd358caa7
Ashwin Phatak <ashwinpphatak@gmail.com>
2019-12-27 18:37:32 +0530

    Provision bond for record publish in tests.

--- src/testing/helper.js
+++ src/testing/helper.js
@@ -5,6 +5,9 @@
 import yaml from 'node-yaml';
 import semver from 'semver';
 
+// Fake bond ID used for in-mem tests.
+const FAKE_BOND_ID = '8e340dd7cf6fc91c27eeefce9cca1406c262e93fd6f3a4f3b1e99b01161fcef3';
+
 export const ensureUpdatedConfig = async path => {
   const conf = await yaml.read(path);
   conf.record.version = semver.inc(conf.record.version, 'patch');
@@ -18,4 +21,24 @@ export const getBaseConfig = async path => {
   conf.record.version = '0.0.1';
 
   return conf;
+};
+
+/**
+ * Provision a bond for record registration.
+ * @param {object} registry
+ * @param {string} privateKey
+ * @param {boolean} mock
+ */
+export const provisionBondId = async (registry, privateKey, mock) => {
+  if (mock) {
+    return FAKE_BOND_ID;
+  }
+
+  let bonds = await registry.queryBonds();
+  if (!bonds.length) {
+    await registry.createBond([{ denom: 'uwire', amount: '1000000000' }], privateKey);
+    bonds = await registry.queryBonds();
+  }
+
+  return bonds[0].id;
 };
